60a20fda38ebfebf2ccbd7310dc23463aabb4731
NaylaHaura <hauranayla27@gmail.com>
2026-06-26 13:31:01 +0700

    add product seeder

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -10,7 +10,7 @@ class ProductSeeder extends Seeder
     public function run(): void
     {
         DB::table('products')->insert([
-            ['name' => 'Produk A', 'link_eksternal' => 'https://tokopedia.com', 'created_at' => now(), 'updated_at' => now()],
+            ['Youthera' => 'House Brands', 'link_eksternal' => 'https://tokopedia.com', 'created_at' => now(), 'updated_at' => now()],
             ['name' => 'Produk B', 'link_eksternal' => 'https://shopee.com', 'created_at' => now(), 'updated_at' => now()],
             ['name' => 'Produk C', 'link_eksternal' => 'https://lazada.com', 'created_at' => now(), 'updated_at' => now()],
         ]);
