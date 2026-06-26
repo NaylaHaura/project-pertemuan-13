7542ae7d261c8a4c767c35cdf16a4445e5e098d9
NaylaHaura <hauranayla27@gmail.com>
2026-06-26 14:10:35 +0700

    fix galeri seeder gambar not null

--- database/seeders/GaleriSeeder.php
+++ database/seeders/GaleriSeeder.php
@@ -10,8 +10,8 @@ class GaleriSeeder extends Seeder
     public function run(): void
     {
         DB::table('galeris')->insert([
-            ['judul' => 'Kantor L\'Essential', 'gambar' => null, 'keterangan' => 'Foto kantor pusat PT L\'Essential.', 'created_at' => now(), 'updated_at' => now()],
-            ['judul' => 'Tim L\'Essential', 'gambar' => null, 'keterangan' => 'Tim profesional PT L\'Essential.', 'created_at' => now(), 'updated_at' => now()],
+            ['judul' => 'Kantor L\'Essential', 'gambar' => 'default.jpg', 'keterangan' => 'Foto kantor pusat PT L\'Essential.', 'created_at' => now(), 'updated_at' => now()],
+            ['judul' => 'Tim L\'Essential', 'gambar' => 'default.jpg', 'keterangan' => 'Tim profesional PT L\'Essential.', 'created_at' => now(), 'updated_at' => now()],
         ]);
     }
 }
